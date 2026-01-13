992ba0ad0761a14a473bf2c2c1a6ebb8e9f81bc5
hugo hao <357566546@qq.com>
2026-01-13 12:01:39 +0800

    用spdk实现了一个echo server

---
+++ spdk_echo_server/spdk_server.d
@@ -0,0 +1,41 @@
+spdk_server.o: spdk_server.c \
+ /home/hugo/learn_spdk/spdk/include/spdk/stdinc.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/thread.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/config.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/fd_group.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/assert.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/cpuset.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/env.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/queue.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/queue_extras.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/pci_ids.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/util.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/fd.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/likely.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/event.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/init.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/log.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/string.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/sock.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/json.h \
+ /home/hugo/learn_spdk/spdk/include/spdk/uuid.h
+/home/hugo/learn_spdk/spdk/include/spdk/stdinc.h:
+/home/hugo/learn_spdk/spdk/include/spdk/thread.h:
+/home/hugo/learn_spdk/spdk/include/spdk/config.h:
+/home/hugo/learn_spdk/spdk/include/spdk/fd_group.h:
+/home/hugo/learn_spdk/spdk/include/spdk/assert.h:
+/home/hugo/learn_spdk/spdk/include/spdk/cpuset.h:
+/home/hugo/learn_spdk/spdk/include/spdk/env.h:
+/home/hugo/learn_spdk/spdk/include/spdk/queue.h:
+/home/hugo/learn_spdk/spdk/include/spdk/queue_extras.h:
+/home/hugo/learn_spdk/spdk/include/spdk/pci_ids.h:
+/home/hugo/learn_spdk/spdk/include/spdk/util.h:
+/home/hugo/learn_spdk/spdk/include/spdk/fd.h:
+/home/hugo/learn_spdk/spdk/include/spdk/likely.h:
+/home/hugo/learn_spdk/spdk/include/spdk/event.h:
+/home/hugo/learn_spdk/spdk/include/spdk/init.h:
+/home/hugo/learn_spdk/spdk/include/spdk/log.h:
+/home/hugo/learn_spdk/spdk/include/spdk/string.h:
+/home/hugo/learn_spdk/spdk/include/spdk/sock.h:
+/home/hugo/learn_spdk/spdk/include/spdk/json.h:
+/home/hugo/learn_spdk/spdk/include/spdk/uuid.h:
